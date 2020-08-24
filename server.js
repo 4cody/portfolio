d093016053ed8d469238c18b3c9d0e553f5176a0
Cody Shaner <cshaner04@gmail.com>
2020-08-24 11:56:41 -0500

    remove root index.html

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
 if (process.env.NODE_ENV === 'production') {
